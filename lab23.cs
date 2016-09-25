9b3df8f14bdf595bd6da531c65515f0624ae34d3
drkttt <graaaace6661@gmail.com>
2016-09-26 00:16:45 +0700

    Add files via upload

---
+++ lab23.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y");
+            int y = int.Parse(Console.ReadLine());
+            var d = (1 - Math.Tan(x));
+            var a = (Math.Cos(x) / Math.Sin(x));
+            var b = (Math.Cos(x - y));
+            var n = d * a + b;
+            Console.WriteLine(n);
+            Console.ReadKey();
+
+        }
+    }
+}
