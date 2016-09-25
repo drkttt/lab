caf1ce4c8b58efe6a70882dd053dd32c80475aac
drkttt <graaaace6661@gmail.com>
2016-09-26 02:24:31 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dat1 = DateTime.Now;
+            Console.WriteLine("{0} дней прошло с начала года", dat1.DayOfYear);
+            int a = dat1.DayOfYear;
+            int b = 365 - a;
+            Console.WriteLine("{0} дней осталось до конца года", b);
+            DateTime ye = new DateTime(dat1.Year, dat1.Month, dat1.Day - 1);
+            Console.WriteLine("Вчера было {0}", ye.ToString("dd MM yyyy"));
+            DateTime to = new DateTime(dat1.Year, dat1.Month, dat1.Day + 1);
+            Console.WriteLine("Завтра будет {0}", to.ToString("dd MM yyyy"));
+            Console.ReadKey();
+        }
+    }
+}
